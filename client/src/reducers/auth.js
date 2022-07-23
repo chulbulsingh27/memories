d96e5eeec743db719c342bfc46fc443094e15800
Chulbul Singh <66539055+chulbulsingh27@users.noreply.github.com>
2022-07-23 11:50:34 +0530

    Add files via upload

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,31 @@
+//import * as actionType from '../constants/actionTypes';
+
+const authReducer = (state = { authData: null }, action) => {
+    switch (action.type) {
+        case 'AUTH':
+            localStorage.setItem('profile', JSON.stringify({ ...action?.data }));
+
+            return { ...state, authData: action?.data};
+        case 'LOGOUT':
+            localStorage.clear();
+
+            return { ...state, authData: null, loading: false, errors: null };
+        default:
+            return state;
+    }
+};
+
+export default authReducer;
+/*const authReducer = (state={authData:null}, action)=>{
+    switch (action.type) {
+        case 'AUTH':
+            console.log(action?.data);
+            return state;
+            
+    
+        default:
+            return state;
+    }
+
+}
+export default authReducer;*/
